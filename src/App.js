b2e633594a6a84c4bc7cc103f42da35e14b22d7b
thantzintun <thantzintun.march201998@gmail.com>
2024-09-07 18:35:37 +0630

    initial_commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello, My name is Thant Zin Tun. I am React Js Developer.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing Deploy React Js Project with Nginx
         </a>
       </header>
     </div>
